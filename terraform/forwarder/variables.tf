a4dc5e89c43b20a87f396f598b2b17737f4bcfc5
Hector Huertas <hectorhuertas@gmail.com>
2022-09-20 11:16:53 +0200

    Update fluentd to 1.15.2

--- terraform/forwarder/variables.tf
+++ terraform/forwarder/variables.tf
@@ -7,7 +7,7 @@ variable "fluentd_image_url" {
 variable "fluentd_image_tag" {
   description = "Fluentd's image tag"
   type        = string
-  default     = "v1.14.6-1"
+  default     = "v1.15.2-1"
 }
 
 variable "aggregator_host" {
