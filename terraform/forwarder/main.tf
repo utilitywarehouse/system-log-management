5a32a7ee9b009b4711f1b9efdada2c5a88eb51a9
DTLP <dlapo@uw.co.uk>
2024-01-24 10:38:02 +0000

    Replace fluentd with promtail

--- terraform/forwarder/main.tf
+++ terraform/forwarder/main.tf
@@ -1,24 +1,24 @@
 data "ignition_file" "config" {
-  mode       = 420
-  path       = "/etc/fluent/fluent.conf"
+  mode = 420
+  path = "/etc/promtail/promtail-config.yaml"
 
   content {
-    content = file("${path.module}/resources/forwarder.conf")
+    content = file("${path.module}/resources/promtail-config.yaml")
   }
 }
 
 data "ignition_systemd_unit" "service" {
-  name = "log-forwarder.service"
+  name = "promtail.service"
 
-  content = templatefile("${path.module}/resources/forwarder.service",
+  content = templatefile("${path.module}/resources/promtail.service",
     {
-      fluentd_image_url   = var.fluentd_image_url
-      fluentd_image_tag   = var.fluentd_image_tag
-      aggregator_host     = var.aggregator_host
-      aggregator_username = var.aggregator_username
-      aggregator_password = var.aggregator_password
-      log_cluster         = var.log_cluster
-      log_machinerole     = var.log_machinerole
+      promtail_image_url = var.promtail_image_url
+      promtail_image_tag = var.promtail_image_tag
+      log_cluster        = var.log_cluster
+      log_machinerole    = var.log_machinerole
+      loki_url           = var.loki_url
+      loki_username      = var.loki_username
+      loki_password      = var.loki_password
     }
   )
 }
